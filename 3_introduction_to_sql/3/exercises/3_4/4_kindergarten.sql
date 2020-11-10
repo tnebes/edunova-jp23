ffa16e4ca67cde1cf40c05815fbd484bd18c60d8
tnebes <47086978+tnebes@users.noreply.github.com>
2020-11-10 19:11:41 +0100

    updated for consistency

--- 3_introduction_to_sql/3/exercises/3_4/4_kindergarten.sql
+++ 3_introduction_to_sql/3/exercises/3_4/4_kindergarten.sql
@@ -4,28 +4,28 @@ create database edunovajp23_3_4_kindergarten;
 use edunovajp23_3_4_kindergarten;
 
 create table edu_group(
-    id int,
-    max_age int,
-    teacher int
+    id          int,
+    max_age     int,
+    teacher     int
 );
 
 create table child(
-    id int,
-    edu_group int
+    id          int,
+    edu_group   int
 );
 
 create table teacher(
-    id int,
-    first_name varchar(50),
-    edu_group int
+    id          int,
+    first_name  varchar(50),
+    edu_group   int
 );
 
 create table qualification(
-    id int,
-    name varchar(100)
+    id          int,
+    name        varchar(100)
 );
 
 create table has_qualification(
-    qualification int,
-    teacher int
+    qualification   int,
+    teacher         int
 );
