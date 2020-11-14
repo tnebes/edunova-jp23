e643d49cc3302e70f5fa5113dc82986c8455ad66
tnebes <47086978+tnebes@users.noreply.github.com>
2020-11-14 18:29:42 +0100

    Started working on Moodle database. God help me.

---
+++ 4_relationships/exercises/moodle/moodle.sql
@@ -0,0 +1,9 @@
+drop database if exists jp23_3_moodle;
+create database jp23_3_moodle;
+
+use jp23_3_moodle;
+
+create table assign(
+   id          bigint(10) primary key not null auto_increment,
+   course      bigint(10) primary key not 
+);
