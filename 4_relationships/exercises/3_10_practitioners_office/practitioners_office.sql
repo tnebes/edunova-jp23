8a1ccb3a6e7867d74c075423ad079cecd0a2c00c
tnebes <47086978+tnebes@users.noreply.github.com>
2020-11-15 15:18:27 +0100

    Updated practitioners office

--- 4_relationships/exercises/3_10_practitioners_office/practitioners_office.sql
+++ 4_relationships/exercises/3_10_practitioners_office/practitioners_office.sql
@@ -3,41 +3,60 @@ create database jp23_3_10_practitioners_office;
 
 use jp23_3_10_practitioners_office;
 
-create table patient(
-	oib			char(13) primary key not null,
-	first_name	varchar(50) not null,
-	last_name	varchar(50) not null
+create table person(
+    oib         char(13) primary key not null,
+    first_name  varchar(50) not null,
+    last_name   varchar(50) not null
 );
 
-create table therapy(
-	id			int primary key not null auto_increment,
-	patient		char(13) not null
+create table employee(
+    person      char(13) primary key not null,
+    iban        varchar(32)
+);
+
+create table doctor(
+    employee    char(13) primary key not null
+);
+
+create table patient(
+	person     char(13) primary key not null
 );
 
 create table treatment(
 	id			int primary key not null auto_increment,
-	therapy		int not null
+    doctor      char(13) not null,
+    patient     char(13) not null,
+    date        datetime not null
 );
 
 create table nurse(
-	id			int primary key not null auto_increment,
-	first_name	varchar(50) not null,
-	last_name	varchar(50) not null
+	employee    char(13) primary key not null
 );
 
 create table nurse_treatment(
-	nurse		int not null,
+	nurse		char(13) not null,
 	treatment	int not null
 );
 
-alter table therapy
-	add foreign key (patient) references patient(oib);
+alter table employee
+    add foreign key (person) references person(oib);
+
+alter table doctor
+    add foreign key (employee) references employee(person);
+
+alter table patient
+    add foreign key (person) references person(oib);
 
 alter table treatment
-	add foreign key (therapy) references therapy(id);
+    add foreign key (doctor) references doctor(employee);
+alter table treatment
+    add foreign key (patient) references patient(person);
+
+alter table nurse
+    add foreign key (employee) references employee(person);
 
 alter table nurse_treatment
-	add foreign key (nurse) references nurse(id);
+	add foreign key (nurse) references nurse(employee);
 alter table nurse_treatment
 	add foreign key (treatment) references treatment(id);
 
