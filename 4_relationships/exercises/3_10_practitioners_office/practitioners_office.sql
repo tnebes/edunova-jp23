e6fcd177a41d99daa7471960c7dfd2cb01a36d31
tnebes <47086978+tnebes@users.noreply.github.com>
2020-11-16 19:45:08 +0100

    Fixed practitioners office according to best practice

--- 4_relationships/exercises/3_10_practitioners_office/practitioners_office.sql
+++ 4_relationships/exercises/3_10_practitioners_office/practitioners_office.sql
@@ -4,59 +4,64 @@ create database jp23_3_10_practitioners_office;
 use jp23_3_10_practitioners_office;
 
 create table person(
-    oib         char(13) primary key not null,
+    id          int primary key not null auto_increment,
+    oib         char(13) not null,
     first_name  varchar(50) not null,
     last_name   varchar(50) not null
 );
 
 create table employee(
-    person      char(13) primary key not null,
+    id          int primary key not null auto_increment,
+    person		int not null,
     iban        varchar(32)
 );
 
 create table doctor(
-    employee    char(13) primary key not null
+    id          int primary key not null auto_increment,
+    employee    int not null
 );
 
 create table patient(
-	person     char(13) primary key not null
+   id         int primary key not null auto_increment,
+	person     int not null
 );
 
 create table treatment(
 	id			int primary key not null auto_increment,
-    doctor      char(13) not null,
-    patient     char(13) not null,
+    doctor      int not null,
+    patient     int not null,
     date        datetime not null
 );
 
 create table nurse(
-	employee    char(13) primary key not null
+   id          int primary key not null,
+	employee    int not null
 );
 
 create table nurse_treatment(
-	nurse		char(13) not null,
+	nurse		   int not null,
 	treatment	int not null
 );
 
 alter table employee
-    add foreign key (person) references person(oib);
+    add foreign key (person) references person(id);
 
 alter table doctor
-    add foreign key (employee) references employee(person);
+    add foreign key (employee) references employee(id);
 
 alter table patient
-    add foreign key (person) references person(oib);
+    add foreign key (person) references person(id);
 
 alter table treatment
-    add foreign key (doctor) references doctor(employee);
+    add foreign key (doctor) references doctor(id);
 alter table treatment
-    add foreign key (patient) references patient(person);
+    add foreign key (patient) references patient(id);
 
 alter table nurse
-    add foreign key (employee) references employee(person);
+    add foreign key (employee) references employee(id);
 
 alter table nurse_treatment
-	add foreign key (nurse) references nurse(employee);
+	add foreign key (nurse) references nurse(id);
 alter table nurse_treatment
 	add foreign key (treatment) references treatment(id);
 
