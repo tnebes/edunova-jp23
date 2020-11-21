4d754266c7aee8b932a44e0319040c9c26a61716
tnebes <47086978+tnebes@users.noreply.github.com>
2020-11-21 14:15:55 +0100

    Wrote Java program that creates a dummy database with dummy data

--- sandbox/java_database_1/JavaDBTest.java
+++ sandbox/java_database_1/JavaDBTest.java
@@ -1,7 +1,7 @@
 /* connect to a database
  * insert dummy values from a list
  */
-package JDBC;
+package JDBC; // do I need this???
 
 import java.sql.*; // we need this to connect to the db
 
@@ -15,36 +15,111 @@ public class JavaDBTest {
       }
       System.out.print("Driver loaded\n");
 
-      String url = "jdbc:mysql://localhost/edunova";
-      String username = "edunova";
-      String password = "edunova";
+      // setting up the connection
+      final String url = "jdbc:mysql://localhost/";
+      final String database_name = "java_db_test";
+      final String username = "edunova";
+      final String password = "edunova";
 
-      Connection connect = DriverManager.getConnection(
+      //connecting to localhost to prepare the database
+      Connection connection = DriverManager.getConnection(
          url, username, password);
+      prepareDatabase(connection, database_name);
 
+      //connecting to the prepared database
+      connection = DriverManager.getConnection(
+         url + database_name, username, password);
+
+      //dummy data for the database
       String[] names = new String[] {"Tomislav", "Lucija", "Petar",
          "Dragan", "Zeljko", "Bobo"};
       String[] bands = new String[] {"Svemirko", "Pocket Palma", "Haustor",
          "Trevor Something", "Led Zeppelin", "Vuco"};
 
-      writeUsers(names);
-      writeBands(bands);
-      userListensBand(names, bands);
+      // creating SQL entities
+      // this is suboptimal lmao
+      String[] entities = new String[] {"person", "band", "person_band"};
+      Statement statement;
+      for (int i = 0; i < entities.length - 1; i++) {
+         statement = connection.createStatement();
+         String sql_statement = "create table " + entities[i] +
+            "(id integer primary key not null auto_increment, " +
+            " name varchar(25) not null)";
+         statement.executeUpdate(sql_statement);
+      }
+      statement = connection.createStatement();
+      statement.executeUpdate("create table " + entities[entities.length - 1] +
+         "(person integer not null, " +
+         " band integer not null)");
+      statement.executeUpdate("alter table " + entities[entities.length - 1] +
+            " add foreign key (band) references band(id)");
+      statement.executeUpdate("alter table " + entities[entities.length - 1] +
+            " add foreign key (person) references person(id)");
+
+
+      writeUsers(connection, names);
+      writeBands(connection, bands);
+      userListensBand(connection, names, bands);
    }
 
-   public static void writeToDatabase() {
-   
+   /** creates or resets a database so that the test can proceed
+    * nominally each time.
+    */
+   public static void prepareDatabase(Connection connection,
+      String database_name) throws SQLException, ClassNotFoundException {
+      Statement statement = connection.createStatement();
+      statement.executeUpdate("drop database if exists java_db_test");
+      statement.executeUpdate("create database java_db_test");
    }
 
-   public static void writeUsers(String[] names) {
-   
+   /** Writes given records to a table. NOTE: When entering data
+    * into an SQL table, DON'T FORGET TO USE SINGLE QUOTES!!!
+    * note(tnebes) this a terrible way to insert records.
+    */
+   public static void insertIntoTable(
+      Connection connection, String table_name,
+      String parameters, String data)
+      throws SQLException, ClassNotFoundException {
+         Statement statement = connection.createStatement();
+         String sql_statement = "insert into " + table_name +
+            "(" + parameters + ") " +
+            "values (" + data + ")";
+         statement.executeUpdate(sql_statement);
    }
 
-   public static void writeBands(String[] bands) {
-   
+   /** Writes users to database 
+    */
+   public static void writeUsers(Connection connection, String[] names)
+      throws SQLException, ClassNotFoundException {
+      for (String name : names) {
+         insertIntoTable(connection, "person", "name", "'" + name + "'");
+      }
    }
 
-   public static void userListensBand(String[] names, String[] bands) {
-   
+   /** Writes bands to database
+    */
+   public static void writeBands(Connection connection, String[] bands)
+      throws SQLException, ClassNotFoundException {
+      for (String band : bands) {
+         insertIntoTable(connection, "band", "name",  "'" + band + "'");
+      }
    }
+
+   /** Establishes relationship between the listeners and
+    * the database
+    */
+   public static void userListensBand(Connection connection,
+      String[] names, String[] bands)
+      throws SQLException, ClassNotFoundException {
+      // randomly create records
+      java.util.Random rng = new java.util.Random();
+      int number_of_records = rng.nextInt(25);
+      for (int i = number_of_records; i > 0; i--) {
+         String data;
+         data = (1 + rng.nextInt(names.length)) + ", " +
+            (1 + rng.nextInt(bands.length));
+         insertIntoTable(connection, "person_band", "person, band", data);
+      }
+   }
+
 }
