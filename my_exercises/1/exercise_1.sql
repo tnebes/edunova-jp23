fef8e0a1c70d2532e4abc3c1178342d9019aa84f
tnebes <47086978+tnebes@users.noreply.github.com>
2020-12-07 16:55:45 +0100

    Inserted values into tables.

--- my_exercises/1/exercise_1.sql
+++ my_exercises/1/exercise_1.sql
@@ -1,99 +1,28 @@
-drop database if exists midterm_exercise_1;
-create database midterm_exercise_1 character set utf8;
+# U tablice muskarac, zena i sestra_svekar unesite po 3 retka.
 
 use midterm_exercise_1;
 
-create table svekar(
-    sifra           int primary key not null auto_increment,
-    bojaociju       varchar(40) not null,
-    prstena         int,
-    dukserica       varchar(41),
-    lipa            decimal(13, 8),
-    eura            decimal(12, 7),
-    majica          varchar(35)
-);
+insert into svekar(bojaociju)
+    values  ('plava'),
+            ('crvena'),
+            ('zelena');
 
-create table sestra(
-    sifra           int primary key not null auto_increment,
-    introvertno     bit,
-    haljina         varchar(31) not null,
-    maraka          decimal(16, 6),
-    hlace           varchar(46) not null,
-    narukvica       int not null
-);
+insert into sestra(haljina, hlace, narukvica)
+    values  ('crvena', 'crvene', 1),
+            ('plava', 'plave', 2),
+            ('zelena', 'zelene', 3);
 
-create table sestra_svekar(
-    sifra           int primary key not null auto_increment,
-    sestra          int not null,
-    svekar          int not null
-);
+insert into sestra_svekar(sestra, svekar)
+    values  (1, 1),
+            (2, 2),
+            (3, 3);
 
-alter table sestra_svekar
-    add foreign key(sestra) references sestra(sifra);
-alter table sestra_svekar
-    add foreign key(svekar) references svekar(sifra);
+insert into zena(jmbag, bojaociju, sestra)
+    values  ('00000000000', 'zelena', 1),
+            ('00000000001', 'smeđa', 2),
+            ('00000000002', 'crna', 3);
 
-create table zena(
-    sifra           int primary key not null auto_increment,
-    treciupta       datetime,
-    hlace           varchar(46),
-    kraktamajica    varchar(31),
-    jmbag           char(11) not null,
-    bojaociju       varchar(39) not null,
-    haljina         varchar(44),
-    sestra          int not null
-);
-
-alter table zena
-    add foreign key (sestra) references sestra(sifra);
-
-create table muskarac(
-    sifra           int primary key not null auto_increment,
-    bojaociju       varchar(50) not null,
-    hlace           varchar(30),
-    modelnaocala    varchar(43),
-    maraka          decimal(14, 5) not null,
-    zena            int not null
-);
-
-alter table muskarac
-    add foreign key (zena) references zena(sifra);
-
-create table mladic(
-    sifra           int primary key not null auto_increment,
-    suknja          varchar(50) not null,
-    kuna            decimal(16, 8) not null,
-    drugiputa       datetime,
-    asocijalno      bit,
-    ekstrovertno    bit not null,
-    dukserica       varchar(48) not null,
-    muskarac        int
-);
-
-alter table mladic
-    add foreign key(muskarac) references muskarac(sifra);
-
-create table punac(
-    sifra           int primary key not null auto_increment,
-    ogrlica         int,
-    gustoca         decimal(14, 9),
-    hlace           varchar(41) not null
-);
-
-create table cura(
-    sifra           int primary key not null auto_increment,
-    novcica         decimal(16, 5) not null,
-    gustoca         decimal(18, 6) not null,
-    lipa            decimal(13, 10),
-    ogrlica         int not null,
-    bojakose        varchar(36),
-    suknja          varchar(36),
-    punac           int
-);
-
-alter table cura
-    add foreign key (punac) references punac(sifra);
-
-## time elapsed: 15:26 ##
-
-
+insert into muskarac(bojaociju, maraka, zena)
+    values  ('smeđa', 100.4, 1),
+            ('crna', 56.4, 2),
+            ('zelena', 0.5, 3);
