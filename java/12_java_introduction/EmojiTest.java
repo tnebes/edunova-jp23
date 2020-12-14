6bcfa15dee98b4deb48ac86b953dbb21ae804bd2
tnebes <47086978+tnebes@users.noreply.github.com>
2020-12-14 18:01:08 +0100

    Attempted to make emoji output

---
+++ java/12_java_introduction/EmojiTest.java
@@ -0,0 +1,7 @@
+public class EmojiTest {
+	public static void main(String[] args) {
+
+		String bear = "\ud83d\udc3b";
+		System.out.println(bear);
+	}
+}
--- java/12_java_introduction/Hello.java
+++ java/12_java_introduction/Hello.java
@@ -2,5 +2,15 @@ public class Hello {
 	public static void main(String[] args) {
 		String name = "Tomislav";
 		System.out.printf("Hello %s!\n", name);
+
+		String[] polaznici = {"Tomislav",
+			"David", "Filip", "Filip", "Gabriela", "Iva",
+			"Ivan", "Ivana", "Marko", "Marko", "Matija",
+			"Natalija", "Srdjan", "Velimir"};
+
+		for (int i = 0; i < polaznici.length; i++) {
+			System.out.printf("Hello, %s!\n", polaznici[i]);
+		}
 	}
 }
+
