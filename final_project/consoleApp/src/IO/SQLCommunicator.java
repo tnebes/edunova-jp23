ca2853d629855b59dc4bba4f7dc15ddbb1e11aea
tnebes <47086978+tnebes@users.noreply.github.com>
2021-02-15 18:06:11 +0100

    Added ability to purge the database. Not sure if that is smart

--- final_project/consoleApp/src/IO/SQLCommunicator.java
+++ final_project/consoleApp/src/IO/SQLCommunicator.java
@@ -1,7 +1,8 @@
 package IO;
 
+import org.mariadb.jdbc.internal.util.exceptions.MariaDbSqlException;
+
 import java.sql.*;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class SQLCommunicator {
@@ -9,9 +10,19 @@ public class SQLCommunicator {
     // add ability to pull from SQL
 
     private static Connection connection;
+    private static String databaseName = "final_project_store_database";
 
     public static void initialise() {
         connection = createConnection();
+        try {
+            System.out.printf("Successfully connected to %s %s at %s .\n",
+                    connection.getMetaData().getDatabaseProductName(),
+                    connection.getMetaData().getDatabaseMajorVersion(),
+                    connection.getMetaData().getURL());
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
     }
 
     private static Connection createConnection() {
@@ -28,9 +39,6 @@ public class SQLCommunicator {
             myConnection = DriverManager.getConnection(tk.nextToken(),
                     tk.nextToken(),
                     tk.nextToken());
-            System.out.printf("Successfully connected to SQL database %s %s.\n",
-                    myConnection.getMetaData().getDatabaseProductName(),
-                    myConnection.getMetaData().getDatabaseMajorVersion());
         } catch (Exception e) {
             e.printStackTrace();
             System.exit(1);
@@ -38,17 +46,53 @@ public class SQLCommunicator {
         return myConnection;
     }
 
-    public static PreparedStatement getQueryResults(String query) {
-        PreparedStatement queryResults = sendQuery(query);
-        return null;
+    private static ResultSet sendQuery(String query) {
+        ResultSet resultSet;
+        try {
+            Statement myStatement = connection.createStatement();
+            resultSet = myStatement.executeQuery(query);
+            return resultSet;
+        } catch (SQLTransientConnectionException e) {
+            e.printStackTrace();
+            return null;
+        } catch (MariaDbSqlException e) {
+            e.printStackTrace();
+            return null;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            connection = createConnection();
+            return sendQuery(query);
+        }
     }
 
-    private static PreparedStatement sendQuery(String query) {
-        return null;
+    public static ResultSet getSQLItems(String entity) {
+        entity = entity.toLowerCase();
+        String SQLQuery;
+        switch (entity) {
+            case "customers": return sendQuery("SELECT * FROM CUSTOMER;");
+            case "addresses": return sendQuery("SELECT * FROM ADDRESS;");
+            case "invoices": return sendQuery("SELECT * FROM INVOICE;");
+            case "articles": return sendQuery("SELECT * FROM ARTICLE;");
+            default: System.out.print("Wrong.\n");
+            return null;
+        }
     }
 
     public static Connection getConnection() {
         return connection;
     }
 
+    public static void updateSQLDatabase() {
+
+    }
+
+    public static void updateLocalDataFromSQL() {
+
+    }
+
+    public static void purgeDatabase() {
+        sendQuery("DROP DATABASE ".concat(databaseName).concat(";"));
+        sendQuery("CREATE DATABASE ".concat(databaseName.concat(" ").concat("CHARACTER SET utf8;")));
+        System.out.printf("Database purged. Populate using SQL script.\n");
+    }
 }
--- final_project/consoleApp/src/UI.java
+++ final_project/consoleApp/src/UI.java
@@ -1,5 +1,6 @@
 
 
+import IO.SQLCommunicator;
 import IO.UserInputHandler;
 import dataHandler.AddressHandler;
 import dataHandler.ArticleHandler;
@@ -7,152 +8,167 @@ import dataHandler.CustomerHandler;
 import dataHandler.InvoiceHandler;
 
 /**
- * 
  * @author tnebes
  * @date 6 February 2021
  */
 
 public class UI {
 
-	// Menu strings.
-	static private String[] mainMenuChoices = { "Invoices", "Customers", "Addresses", "Articles", "Options", "Exit" };
-	static private String[] genericManipulation = { "Show", "Add", "Change", "Delete", "Exit" };
-	static private String notification = "=== Drau ElektroSoftware GmbH ===";
+    // Menu strings.
+    static private String[] mainMenuChoices = {"Invoices", "Customers", "Addresses", "Articles", "Database", "Exit"};
+    static private String[] genericManipulation = {"Show", "Add", "Change", "Delete", "Exit"};
+    static private String notification = "=== Drau ElektroSoftware GmbH ===";
 
-	// value of ints is not index but what user sees. e.g. invoices is 1 and not 0.
-	static final int OPTIONS = 5;
-	static final int EXIT = 6;
+    // value of ints is not index but what user sees. e.g. invoices is 1 and not 0.
+    static final int SQL = 5;
+    static final int EXIT = 6;
 
-	public static void mainMenu() {
-		System.out.printf("%s\n\n", notification);
-		int userChoice;
-		do {
-			System.out.print("\n");
-			for (int i = 0; i < mainMenuChoices.length; i++) {
-				// i + 1 for proper user choice
-				System.out.printf("%d - %s\n", i + 1, mainMenuChoices[i]);
-			}
-			System.out.print("\nChoice: ");
-			userChoice = (int) UserInputHandler.getIntegerInput(true);
-			// should the user enter rubbish
-			if (userChoice <= 0 || userChoice > mainMenuChoices.length) {
-				System.out.printf("Input must be from 1 to %d\n", mainMenuChoices.length);
-				continue;
-			}
-			// proceed
-			if (userChoice != mainMenuChoices.length) {
-				subMenu(userChoice);
-			}
-			// exit
-			else {
-				System.out.printf("\n\n%s\n", notification);
-				System.exit(0);
-			}
-		} while (true);
-	}
+    public static void mainMenu() {
+        System.out.printf("%s\n\n", notification);
+        int userChoice;
+        do {
+            System.out.print("\n");
+            for (int i = 0; i < mainMenuChoices.length; i++) {
+                // i + 1 for proper user choice
+                System.out.printf("%d - %s\n", i + 1, mainMenuChoices[i]);
+            }
+            System.out.print("\nChoice: ");
+            userChoice = (int) UserInputHandler.getIntegerInput(true);
+            // should the user enter rubbish
+            if (userChoice <= 0 || userChoice > mainMenuChoices.length) {
+                System.out.printf("Input must be from 1 to %d\n", mainMenuChoices.length);
+                continue;
+            }
+            // proceed
+            if (userChoice != mainMenuChoices.length) {
+                subMenu(userChoice);
+            }
+            // exit
+            else {
+                System.out.printf("\n\n%s\n", notification);
+                System.exit(0);
+            }
+        } while (true);
+    }
 
-	private static void subMenu(int previousMenu) {
-		int userInput;
-		// options will have a different menu
-		if (previousMenu == OPTIONS) {
-			// TODO temporary
-			System.exit(1);
-		}
-		do {
-			System.out.print("\n");
-			for (int i = 0; i < genericManipulation.length; i++) {
-				// i - 1 required for proper rendering of options.
-				System.out.printf("%d - %s %s\n", i + 1, genericManipulation[i], mainMenuChoices[previousMenu - 1]);
-			}
-			System.out.print("\nChoice: ");
-			userInput = (int) UserInputHandler.getIntegerInput(true);
-			if (userInput <= 0 || userInput > genericManipulation.length) {
-				System.out.printf("Input must be from 1 to %d\n", genericManipulation.length);
-				continue;
-			}
-			// submenu choices
-			switch (userInput) {
-			// show
-			case 1:
-				// if invoices
-				if (previousMenu == 1) {
-					InvoiceHandler.showInvoices();
-				}
-				// if customers
-				else if (previousMenu == 2) {
-					CustomerHandler.showCustomers();
-				}
-				// if addresses
-				else if (previousMenu == 3) {
-					AddressHandler.showAddresses();
-				}
-				// if articles
-				else if (previousMenu == 4) {
-					ArticleHandler.showArticles();
-				}
-				break;
-			// add
-			case 2:
-				// if invoices
-				if (previousMenu == 1) {
-					InvoiceHandler.addInvoice();
-				}
-				// if customers
-				else if (previousMenu == 2) {
-					CustomerHandler.addCustomer();
-				}
-				// if addresses
-				else if (previousMenu == 3) {
-					AddressHandler.addAddress();
-				}
-				// if articles
-				else if (previousMenu == 4) {
-					ArticleHandler.addArticle();
-				}
-				break;
-			// change
-			case 3:
-				// if invoices
-				if (previousMenu == 1) {
-					InvoiceHandler.changeInvoice();
-				}
-				// if customers
-				else if (previousMenu == 2) {
-					CustomerHandler.changeCustomer();
-				}
-				// if addresses
-				else if (previousMenu == 3) {
-					AddressHandler.changeAddress();
-				}
-				// if articles
-				else if (previousMenu == 4) {
-					ArticleHandler.changeArticle();
-				}
-				break;
-			// delete
-			case 4:
-				// if invoices
-				if (previousMenu == 1) {
-					InvoiceHandler.deleteInvoice();
-				}
-				// if customers
-				else if (previousMenu == 2) {
-					CustomerHandler.deleteCustomer();
-				}
-				// if addresses
-				else if (previousMenu == 3) {
-					AddressHandler.deleteAddress();
-				}
-				// if articles
-				else if (previousMenu == 4) {
-					ArticleHandler.deleteArticle();
-				}
-				break;
-			case 5:
-				System.out.print("\n");
-				return;
-			}
-		} while (true);
-		// options menu
-	}
+    private static void subMenu(int previousMenu) {
+        int userInput;
+        // options will have a different menu
+        if (previousMenu == SQL) {
+            System.out.printf("1 - update database\n2 - load from database\n3 - purge database\n0 - return\n");
+            userInput = (int) UserInputHandler.getIntegerInput(false);
+            if (userInput < 1 || userInput > 3) {
+                return;
+            }
+            switch (userInput) {
+                case 1:
+                    SQLCommunicator.updateSQLDatabase();
+                    return;
+                case 2:
+                    SQLCommunicator.updateLocalDataFromSQL();
+                    return;
+                case 3:
+                    SQLCommunicator.purgeDatabase();
+                    return;
+                default:
+                    return;
+            }
+        }
+        do {
+            System.out.print("\n");
+            for (int i = 0; i < genericManipulation.length; i++) {
+                // i - 1 required for proper rendering of options.
+                System.out.printf("%d - %s %s\n", i + 1, genericManipulation[i], mainMenuChoices[previousMenu - 1]);
+            }
+            System.out.print("\nChoice: ");
+            userInput = (int) UserInputHandler.getIntegerInput(true);
+            if (userInput <= 0 || userInput > genericManipulation.length) {
+                System.out.printf("Input must be from 1 to %d\n", genericManipulation.length);
+                continue;
+            }
+            // submenu choices
+            switch (userInput) {
+                // show
+                case 1:
+                    // if invoices
+                    if (previousMenu == 1) {
+                        InvoiceHandler.showInvoices();
+                    }
+                    // if customers
+                    else if (previousMenu == 2) {
+                        CustomerHandler.showCustomers();
+                    }
+                    // if addresses
+                    else if (previousMenu == 3) {
+                        AddressHandler.showAddresses();
+                    }
+                    // if articles
+                    else if (previousMenu == 4) {
+                        ArticleHandler.showArticles();
+                    }
+                    break;
+                // add
+                case 2:
+                    // if invoices
+                    if (previousMenu == 1) {
+                        InvoiceHandler.addInvoice();
+                    }
+                    // if customers
+                    else if (previousMenu == 2) {
+                        CustomerHandler.addCustomer();
+                    }
+                    // if addresses
+                    else if (previousMenu == 3) {
+                        AddressHandler.addAddress();
+                    }
+                    // if articles
+                    else if (previousMenu == 4) {
+                        ArticleHandler.addArticle();
+                    }
+                    break;
+                // change
+                case 3:
+                    // if invoices
+                    if (previousMenu == 1) {
+                        InvoiceHandler.changeInvoice();
+                    }
+                    // if customers
+                    else if (previousMenu == 2) {
+                        CustomerHandler.changeCustomer();
+                    }
+                    // if addresses
+                    else if (previousMenu == 3) {
+                        AddressHandler.changeAddress();
+                    }
+                    // if articles
+                    else if (previousMenu == 4) {
+                        ArticleHandler.changeArticle();
+                    }
+                    break;
+                // delete
+                case 4:
+                    // if invoices
+                    if (previousMenu == 1) {
+                        InvoiceHandler.deleteInvoice();
+                    }
+                    // if customers
+                    else if (previousMenu == 2) {
+                        CustomerHandler.deleteCustomer();
+                    }
+                    // if addresses
+                    else if (previousMenu == 3) {
+                        AddressHandler.deleteAddress();
+                    }
+                    // if articles
+                    else if (previousMenu == 4) {
+                        ArticleHandler.deleteArticle();
+                    }
+                    break;
+                case 5:
+                    System.out.print("\n");
+                    return;
+            }
+        } while (true);
+        // options menu
+    }
 }
